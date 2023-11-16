8c951d3e1c8eaf441a7f167bff0e52aeaf54a61f
nguyenvandung1 <78417712+nguyenvandung1@users.noreply.github.com>
2023-11-16 22:49:55 +0700

    AnLonNenUplai

--- navigation/AuthStack.js
+++ navigation/AuthStack.js
@@ -12,13 +12,12 @@ function AuthStack() {
             {/* <Stack.Screen name="HomeScreen" component={HomeScreen}/> */}
             {/* <Stack.Screen name='HomeUserStack' component={HomeUserStack}/> */}
             
-            <Stack.Screen name="HomeScreen" component={User}/>
             <Stack.Screen name="OnBoardingScreen" component={OnBoardingScreen} />
             <Stack.Screen name="LoginScreen" component={LoginScreen} />
             <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
             <Stack.Screen name="ForgetScreen" component={ForgetScreen} />
             <Stack.Screen name="ChangePassScreen" component={ChangePassScreen} />
-            
+            <Stack.Screen name="HomeScreen" component={User}/>
         </Stack.Navigator>
     )
 }
